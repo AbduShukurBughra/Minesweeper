9397859dd404b632bd0ac0503d9f3ffeb057158a
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2023-01-26 09:39:05 +0300

    GameWin Class

--- src/com/bughra/GameWin.java
+++ src/com/bughra/GameWin.java
@@ -1,2 +1,27 @@
-package com.bughra;public class GameWin {
+package com.bughra;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWin extends JFrame {
+
+    void launch(){
+        this.setVisible(true);
+        this.setSize(500, 500);
+        this.setLocale(null);
+        this.setTitle("مىنا تازلاش ئويۇنى");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.drawLine(100,100,100,400);
+        g.drawLine(100,100,400,100);
+    }
+
+    public static void main(String[] args) {
+        GameWin gameWin = new GameWin();
+        gameWin.launch();
+    }
 }
